3dae2e87d5107b7468dfe881d4e865924a356142
Mark Mwima <markmwimar@gmail.com>
2024-08-06 21:04:03 +0300

    question constructor

--- QuestionService.java
+++ QuestionService.java
@@ -3,7 +3,8 @@ public class QuestionService {
     Question[] questions = new Question[5];
 
     public void displayQuestions() {
-        
+        questions[0] = new Question();
+        System.out.println(questions[0].getQuestion());
     }
 
 }

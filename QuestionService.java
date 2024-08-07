ad94aa990f81ea8897624d7a3223bdfc532fbcfb
Mark Mwima <markmwimar@gmail.com>
2024-08-07 13:07:56 +0300

    checking each answes

--- QuestionService.java
+++ QuestionService.java
@@ -7,12 +7,11 @@ public class QuestionService {
     String selection[] = new String[5];
 
     public QuestionService() {
-        questions[0] = new Question(1, "What is Int", "Framework", "Library", "Language", "Class", "Language");
-        questions[1] = new Question(1, "What is Java", "primitive", "Library", "Language", "Class", "primitive");
-        questions[2] = new Question(1, "What is Object", "Framework", "Reference", "Language", "Class", "Reference");
-        questions[3] = new Question(1, "What is String", "Framework", "nonprimitive", "Language", "Class",
-                "nonprimitive");
-        questions[4] = new Question(1, "What is Main", "Class", "Library", "Language", "object", "Class");
+        questions[0] = new Question(1, "How Many legs Cow?", "6", "4", "1", "7", "4");
+        questions[1] = new Question(2, "How Many legs ant?", "3", "2", "6", "10", "6");
+        questions[2] = new Question(3, "How Many legs Man?", "2", "3", "8", "6", "2");
+        questions[3] = new Question(4, "How Many legs Spider?", "8", "5", "2", "1","8");
+        questions[4] = new Question(5, "How Many legs Snake", "0", "8", "10", "3", "0");
 
         // Question q1 = new Question();
 
@@ -35,10 +34,20 @@ public class QuestionService {
             selection[i] = sc.next();
             i++;
 
-            for(String s : selection){
-                System.out.println(s);
-            }
-        } 
+        }
+        for (String s : selection) {
+            System.out.println(s);
+        }
+    }
+    
+    public void printScore() {
+        int score = 0;
+        for (int i = 0; i < questions.length; i++) {
+            Question que = questions[i];
+            String answer = que.getAnswer();
+
+            
+        }
     }
 
 }

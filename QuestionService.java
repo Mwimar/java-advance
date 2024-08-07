575cd0d036cb0457d81a9d96f3e702b643e5b794
Mark Mwima <markmwimar@gmail.com>
2024-08-07 12:31:38 +0300

    printing questions and answers

--- QuestionService.java
+++ QuestionService.java
@@ -6,10 +6,10 @@ public class QuestionService {
         questions[0] = new Question(1, "What is Int", "Framework", "Library", "Language", "Class", "Language");
         questions[1] = new Question(1, "What is Java", "primitive", "Library", "Language", "Class", "primitive");
         questions[2] = new Question(1, "What is Object", "Framework", "Reference", "Language", "Class", "Reference");
-        questions[3] = new Question(1, "What is String", "Framework", "nonprimitive", "Language", "Class", "nonprimitive");
+        questions[3] = new Question(1, "What is String", "Framework", "nonprimitive", "Language", "Class",
+                "nonprimitive");
         questions[4] = new Question(1, "What is Main", "Class", "Library", "Language", "object", "Class");
-        
-            
+
         // Question q1 = new Question();
 
         // questions[1] = new Question();
@@ -17,10 +17,15 @@ public class QuestionService {
 
     }
 
-    public void displayQuestions() {
+    public void playQuiz() {
         for (Question q : questions) {
-            
-            System.out.println(q.getAnswer());
+
+            System.out.println("Question no:" + q.getId());
+            System.out.println(q.getQuestion());
+            System.out.println(q.getOpt1());
+            System.out.println(q.getOpt2());
+            System.out.println(q.getOpt3());
+            System.out.println(q.getOpt4());
         }
     }
 
